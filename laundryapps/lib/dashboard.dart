6306132a1750fcf31b70ef1155318eed216d1b85
m1abdat <abdatmisel@gmail.com>
2024-06-08 14:29:14 +0700

    login

---
+++ laundryapps/lib/dashboard.dart
@@ -0,0 +1,55 @@
+// dashboard.dart
+
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page'),
+    Text('Search Page'),
+    Text('Profile Page'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- laundryapps/lib/login.dart
+++ laundryapps/lib/login.dart
@@ -1,4 +1,23 @@
+// main.dart
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dashboard.dart'; // Import dashboard.dart
+
+void main() {
+  runApp(LoginApp());
+}
+
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      home: LoginPage(), // Set LoginPage as the home page
+    );
+  }
+}
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,61 +25,101 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  bool _isPasswordVisible = false;
-  TextEditingController _passwordController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future<void> _login() async {
+    String url = 'https://dummyjson.com/user/login';
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+    };
+    Map<String, dynamic> body = {
+      'username': usernameController.text,
+      'password': passwordController.text,
+      'expiresInMins': 30, // optional, defaults to 60
+    };
+
+    try {
+      http.Response response = await http.post(Uri.parse(url),
+          headers: headers, body: jsonEncode(body));
+
+      if (response.statusCode == 200) {
+        // Successful login
+        Map<String, dynamic> data = jsonDecode(response.body);
+        print(data); // You can handle the response data here
+
+        // Directly navigate to DashboardPage
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => DashboardPage()),
+        );
+      } else {
+        // Failed login
+        print('Login failed. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error occurred: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-              ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          image: DecorationImage(
+            image: AssetImage("assets/logo_clothing.png"),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.3),
+              BlendMode.dstATop,
             ),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: !_isPasswordVisible,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Welcome to Our App',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                  onPressed: () {
-                    setState(() {
-                      _isPasswordVisible = !_isPasswordVisible;
-                    });
-                  },
                 ),
-              ),
+                SizedBox(height: 20.0),
+                TextField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: _login,
+                  child: Text('Login'),
+                ),
+              ],
             ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Tambahkan logika untuk verifikasi login di sini
-                String email = 'example@example.com'; // Ganti dengan email yang valid
-                String password = 'password'; // Ganti dengan password yang valid
-
-                if (_passwordController.text == password) {
-                  // Jika password benar, tambahkan navigasi ke halaman selanjutnya
-                  // Misalnya, Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-                  print('Login berhasil');
-                } else {
-                  print('Login gagal');
-                }
-              },
-              child: Text('Login'),
-            ),
-          ],
+          ),
         ),
       ),
     );
